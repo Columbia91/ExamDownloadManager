bf38515d74f9949ccb052ec91e7f5045d1b47238
Columbia91 <alpha.z91@mail.ru>
2019-07-03 21:49:43 +0600

    Добавьте файлы проекта.

---
+++ DownloadManager/Abc.cs
@@ -0,0 +1,20 @@
+﻿namespace DownloadManager
+{
+    public class Abc
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public string Part { get; set; }
+        public string FileName { get; set; }
+        public string FileFormat { get; set; }
+
+        public Abc(int from, int to, string part, string fileName, string fileFormat)
+        {
+            From = from;
+            To = to;
+            Part = part;
+            FileName = fileName;
+            FileFormat = fileFormat;
+        }
+    }
+}
---
+++ DownloadManager/DownloadForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace DownloadManager
+{
+    partial class DownloadForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFileFormat = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtThreadCount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL адрес";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(98, 23);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(483, 20);
+            this.txtAddress.TabIndex = 1;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(98, 104);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(483, 20);
+            this.txtPath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Сохранить в";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(98, 148);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(483, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(288, 184);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(79, 13);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Загружено 0%";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(410, 208);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(506, 208);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(587, 101);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(31, 23);
+            this.btnBrowse.TabIndex = 8;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Имя файла";
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(98, 62);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(299, 20);
+            this.txtFileName.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(403, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Расширение";
+            // 
+            // txtFileFormat
+            // 
+            this.txtFileFormat.Location = new System.Drawing.Point(479, 62);
+            this.txtFileFormat.Name = "txtFileFormat";
+            this.txtFileFormat.Size = new System.Drawing.Size(102, 20);
+            this.txtFileFormat.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Кол-во потоков";
+            // 
+            // txtThreadCount
+            // 
+            this.txtThreadCount.Location = new System.Drawing.Point(127, 210);
+            this.txtThreadCount.Name = "txtThreadCount";
+            this.txtThreadCount.Size = new System.Drawing.Size(100, 20);
+            this.txtThreadCount.TabIndex = 14;
+            // 
+            // DownloadForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 268);
+            this.Controls.Add(this.txtThreadCount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFileFormat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "DownloadForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Download";
+            this.Load += new System.EventHandler(this.DownloadForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFileFormat;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtThreadCount;
+    }
+}
